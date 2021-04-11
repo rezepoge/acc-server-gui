bba59aa1417ac06d284e42c370ed193763ffa6f3
Thomas Middelveld <thomas.middelveld@reze.li>
2021-04-11 17:19:32 +0200

    Update + trim fix

--- src/ConfigProvider.js
+++ src/ConfigProvider.js
@@ -13,13 +13,37 @@ const files = {
 let config = {};
 
 const load = () => {
-    config = {
-        settings: fs.existsSync(files.settings) ? JSON.parse(fs.readFileSync(files.settings, {encoding: 'utf16le'}).toString()) : {},
-        configuration: fs.existsSync(files.configuration) ? JSON.parse(fs.readFileSync(files.configuration, {encoding: 'utf16le'}).toString()) : {},
-        event: fs.existsSync(files.event) ? JSON.parse(fs.readFileSync(files.event, {encoding: 'utf16le'}).toString()) : {},
-        eventRules: fs.existsSync(files.eventRules) ? JSON.parse(fs.readFileSync(files.eventRules, {encoding: 'utf16le'}).toString()) : {},
-        assistRules: fs.existsSync(files.assistRules) ? JSON.parse(fs.readFileSync(files.assistRules, {encoding: 'utf16le'}).toString()) : {},
-    };
+    const settingsData = fs.existsSync(files.settings) ? fs.readFileSync(files.settings, {
+        encoding: 'utf16le'
+    }).toString().trim() : '';
+
+    const configurationData = fs.existsSync(files.configuration) ? fs.readFileSync(files.configuration, {
+        encoding: 'utf16le'
+    }).toString().trim() : '';
+
+    const eventData = fs.existsSync(files.event) ? fs.readFileSync(files.event, {
+        encoding: 'utf16le'
+    }).toString().trim() : '';
+
+    const eventRulesData = fs.existsSync(files.eventRules) ? fs.readFileSync(files.eventRules, {
+        encoding: 'utf16le'
+    }).toString().trim() : '';
+
+    const assistRulesData = fs.existsSync(files.assistRules) ? fs.readFileSync(files.assistRules, {
+        encoding: 'utf16le'
+    }).toString().trim() : '';
+
+    try {
+        config = {
+            settings: JSON.parse(settingsData),
+            configuration: JSON.parse(configurationData),
+            event: JSON.parse(eventData),
+            eventRules: JSON.parse(eventRulesData),
+            assistRules: JSON.parse(assistRulesData),
+        };
+    } catch (ex) {
+        console.error(ex.stack);
+    }
 }
 
 const save = (newconfig) => {
@@ -29,11 +53,21 @@ const save = (newconfig) => {
     newconfig.configuration.lanDiscovery = 1;
     newconfig.configuration.configVersion = 1;
     newconfig.event.configVersion = 1;
-    fs.writeFileSync(files.settings, JSON.stringify(newconfig.settings, undefined, 4), {encoding: 'utf16le'});
-    fs.writeFileSync(files.configuration, JSON.stringify(newconfig.configuration, undefined, 4), {encoding: 'utf16le'});
-    fs.writeFileSync(files.event, JSON.stringify(newconfig.event, undefined, 4), {encoding: 'utf16le'});
-    fs.writeFileSync(files.eventRules, JSON.stringify(newconfig.eventRules, undefined, 4), {encoding: 'utf16le'});
-    fs.writeFileSync(files.assistRules, JSON.stringify(newconfig.assistRules, undefined, 4), {encoding: 'utf16le'});
+    fs.writeFileSync(files.settings, JSON.stringify(newconfig.settings, undefined, 4), {
+        encoding: 'utf16le'
+    });
+    fs.writeFileSync(files.configuration, JSON.stringify(newconfig.configuration, undefined, 4), {
+        encoding: 'utf16le'
+    });
+    fs.writeFileSync(files.event, JSON.stringify(newconfig.event, undefined, 4), {
+        encoding: 'utf16le'
+    });
+    fs.writeFileSync(files.eventRules, JSON.stringify(newconfig.eventRules, undefined, 4), {
+        encoding: 'utf16le'
+    });
+    fs.writeFileSync(files.assistRules, JSON.stringify(newconfig.assistRules, undefined, 4), {
+        encoding: 'utf16le'
+    });
 }
 
 load();
