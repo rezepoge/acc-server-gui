b05f5c0db7b1b2dfd3ff97e4ee615fabcaac5828
Thomas Middelveld <thomas.middelveld@reze.li>
2021-05-26 18:17:07 +0200

    0.2.8 - laptime table - dont show passwords - some css improvements

--- public/js/results.js
+++ public/js/results.js
@@ -14,11 +14,28 @@ function renderTimetable(ev, elem, index) {
             elem2.classList.remove('active');
         }
     });
-    let timetableHtml = '';
+    let timetableHtml = `<div class="timeTableLine_header">
+        <div class="driver">
+            Driver
+        </div>
+        <div class="car">
+            Car
+        </div>
+        <div class="timing">
+            <div>Best</div>
+            <div>Last</div>
+            <div>Avg</div>
+            <div>Total</div>
+        </div>
+    </div>`;
+
     results[index].sessionResult.leaderBoardLines.forEach((line, position) => {
-        timetableHtml += `<div class="timeTableLine${line.timing.totalTime <= line.timing.bestLap ? ' noValidLap' : ''}">
+        const lapsTimesByDriver = results[index].laps.filter(elem => elem.carId === line.car.carId).map(lap => lap.laptime);
+        const avgLap = lapsTimesByDriver.length ? (lapsTimesByDriver.reduce((sum, val) => sum + val, 0) / lapsTimesByDriver.length) : null;
+
+        timetableHtml += `<div class="timeTableLine${line.timing.totalTime <= line.timing.bestLap ? ' noValidLap' : ''}" data-carId="${line.car.carId}" data-position="${position}">
                 <div class="driver">
-                    <div class="position">${pad(position+1, 2)}</div>
+                    <div class="position">${position+1}</div>
                     <div class="shortName" title="${line.currentDriver.firstName} ${line.currentDriver.lastName}">${line.currentDriver.shortName}</div>
                 </div>
                 <div class="car">
@@ -26,20 +43,113 @@ function renderTimetable(ev, elem, index) {
                     <div class="carModel">${basics.cars.find(car => car.id === line.car.carModel).name}</div>
                 </div>
                 <div class="timing">
-                    <div class="bestLap">${getTime(line.timing.bestLap)}</div>
-                    <div class="lastLap${line.timing.bestLap === line.timing.lastLap ? ' bestLap' : ''}">${getTime(line.timing.lastLap)}</div>
-                    <div class="totalTime">${getTime(line.timing.totalTime)}</div>
+                    <div class="${position === 0 ? ' bestLapAll' : 'bestLap'}" title="Best laptime">${getTime(line.timing.bestLap)}</div>
+                    <div class="lastLap${line.timing.bestLap === line.timing.lastLap ? ' bestLap' : ''}" title="Last laptime">${getTime(line.timing.lastLap)}</div>
+                    <div class="avgLap" title="Average laptime">${getTime(avgLap)}</div>
+                    <div class="totalTime" title="Total time">${getTime(line.timing.totalTime)}</div>
                 </div>
             </div>`
     });
 
     timetable.innerHTML = timetableHtml;
+
+    Array.from(timetable.querySelectorAll('.timeTableLine:not(.noValidLap)'))
+        .forEach(elem => elem.addEventListener('click', ev => renderLaps(ev, elem, index, timetable)));
+
     timetable.scrollIntoView({
         behavior: 'smooth'
     });
 }
 
+function renderLaps(ev, elem, index, timetable) {
+    const carId = parseInt(elem.getAttribute('data-carId'));
+    const position = parseInt(elem.getAttribute('data-position'));
+
+    const oldLapsElem = document.querySelector(`#laps_${carId}.laps`);
+
+    if (oldLapsElem) {
+        oldLapsElem.remove();
+        return;
+    }
+
+    const lapsElem = document.createElement('div');
+    lapsElem.className = 'laps';
+    lapsElem.id = 'laps_' + carId;
+
+    let lapTableHtml = `<div class="lapTableLine_header">
+        <div>
+            Lap
+        </div>
+        <div>
+            Laptime
+        </div>
+        <div class="splits">
+            <div>S1</div>
+            <div>S2</div>
+            <div>S3</div>
+        </div>
+    </div>`;
+
+    const eventResults = results[index];
+    const leaderBoardLines = eventResults.sessionResult.leaderBoardLines;
+    const lapsByDriver = eventResults.laps
+        .filter(elem => elem.carId === carId);
+
+    const bestLap = leaderBoardLines[position].timing.bestLap;
+    const bestLapAll = Math.min(...eventResults.laps
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.laptime));
+
+    const bestFirstSplit = Math.min(...lapsByDriver
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[0]));
+    const bestFirstSplitAll = Math.min(...eventResults.laps
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[0]));
+
+    const bestSecondSplit = Math.min(...lapsByDriver
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[1]));
+    const bestSecondSplitAll = Math.min(...eventResults.laps
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[1]));
+
+    const bestThirdSplit = Math.min(...lapsByDriver
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[2]));
+    const bestThirdSplitAll = Math.min(...eventResults.laps
+        .filter(lap => lap.isValidForBest)
+        .map(lap => lap.splits[2]));
+
+    lapsByDriver.forEach((lap, number) => {
+        const laptimeAddClass = lap.laptime === bestLapAll ? 'bestLapAll' :
+            lap.laptime === bestLap ? 'bestLap' : '';
+        const firstSplitAddClass = lap.splits[0] === bestFirstSplitAll ? 'bestLapAll' :
+            lap.splits[0] === bestFirstSplit ? 'bestLap' : '';
+        const secondSplitAddClass = lap.splits[1] === bestSecondSplitAll ? 'bestLapAll' :
+            lap.splits[1] === bestSecondSplit ? 'bestLap' : '';
+        const thirdSplitAddClass = lap.splits[2] === bestThirdSplitAll ? 'bestLapAll' :
+            lap.splits[2] === bestThirdSplit ? 'bestLap' : '';
+
+        lapTableHtml += `<div class="lapTableLine${!lap.isValidForBest ? ' noValidLap' : ''}">
+                <div class="lap">${number+1}</div>
+                <div class="laptime ${laptimeAddClass}">${getTime(lap.laptime)}</div>
+                <div class="splits">
+                    <div class="laptime ${firstSplitAddClass}">${getTime(lap.splits[0])}</div>
+                    <div class="laptime ${secondSplitAddClass}">${getTime(lap.splits[1])}</div>
+                    <div class="laptime ${thirdSplitAddClass}">${getTime(lap.splits[2])}</div>
+                </div>
+            </div>`
+    });
+
+    lapsElem.innerHTML = lapTableHtml;
+    timetable.insertBefore(lapsElem, elem.nextElementSibling);
+}
+
 function getTime(total) {
+    if (!total) {
+        return '--:--.---';
+    }
     const millis = total % 1000;
     const seconds = ((total - millis) / 1000) % 60;
     const minutes = ((((total - millis) / 1000) - seconds) / 60) % 60;
